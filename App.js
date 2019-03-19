379442745efbbe3fb0c4773bae213ea135fafd9f
vishal marhatta <vishal.marhatta@gmail.com>
2019-03-20 04:49:38 +0530

    Added basic map with initial region

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
+
+	state={
+		region:{
+			latitude:37.78825,
+      		longitude:-122.4324,
+      		latitudeDelta:0.0922,
+      		longitudeDelta:0.0421
+		}
+	}
+
+	region = this.state.region;
+
+
+	onRegionChange=(region)=>{
+		this.setState({region})
+		console.log(region);
+	}
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    	<View style={styles.container}>
+      		<MapView
+      		style={{flex:1}}
+      		initialRegion={this.state.region}
+      		onRegionChange={this.onRegionChange}
+      		/>
+      	</View>
+      	);
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GoogleMapsWithReactNative
+# test Repo
