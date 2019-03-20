3018030728147983d447022c9737fae705249b96
vishal marhatta <vishal.marhatta@gmail.com>
2019-03-20 07:18:11 +0530

    GPS issues.Need to turn on location somehow

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Location,Permissions} from 'expo';
 import MapView from 'react-native-maps';
+
 
 export default class App extends React.Component {
 
 	state={
 		region:{
-			latitude:37.78825,
-      		longitude:-122.4324,
+			latitude:18.4907,
+      		longitude:73.8093,
       		latitudeDelta:0.0922,
       		longitudeDelta:0.0421
-		}
+		},
+		hasLocationPermissions:false,
+		location:null
 	}
 
 	region = this.state.region;
@@ -21,6 +25,29 @@ export default class App extends React.Component {
 		console.log(region);
 	}
 
+	componentDidMount(){
+
+		this._getLocationAsync();
+	}
+
+
+	_getLocationAsync = async ()=>{
+		
+		const {status,permissions} = await Permissions.askAsync(Permissions.LOCATION);
+			if(status === 'granted'){
+				this.setState({hasLocationPermissions:true});
+				return await Location.getCurrentPositionAsync({enableHighAccuracy:true})
+				
+				// this.setState({location});
+				// console.log(location);
+			} else {
+				throw new Error('Location permission not granted');
+			}
+
+			
+	
+	}
+
   render() {
     return (
     	<View style={styles.container}>
@@ -28,6 +55,7 @@ export default class App extends React.Component {
       		style={{flex:1}}
       		initialRegion={this.state.region}
       		onRegionChange={this.onRegionChange}
+      		showsUserLocation
       		/>
       	</View>
       	);
